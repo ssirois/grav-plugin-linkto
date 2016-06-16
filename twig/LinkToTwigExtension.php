3b9ca80af24bc42f33f09dc6b2b0541ee5a3a793
Patrick P. Henley <patrick.p.henley@gmail.com>
2016-06-16 17:53:56 -0400

    Formated php code to make it more PSR-1 and PSR-2 compliant.

--- twig/LinkToTwigExtension.php
+++ twig/LinkToTwigExtension.php
@@ -3,79 +3,81 @@ namespace Grav\Plugin;
 
 class LinkToTwigExtension extends \Twig_Extension
 {
-  /**
-  * Returns extension name.
-  *
-  * @return string
-  */
-  public function getName()
-  {
-    return 'LinkToTwigExtension';
-  }
-
-  public function getFilters()
-  {
-    return [
-      new \Twig_SimpleFilter('link_to', [$this, 'linkTo']),
-    ];
-  }
-
-  public function getFunctions()
-  {
-    return [
-      new \Twig_SimpleFunction('link_to', [$this, 'linkTo']),
-    ];
-  }
-
-  public function linkTo($raw, $options = array())
-  {
-    switch(gettype($raw)) {
-      case 'object':
-        $raw = array('page' => $raw);
-        break;
-      case 'string':
-        $raw = array('content' => $raw);
-        break;
-      case 'array':
-        break;
-      default:
-        $raw = array();
+    /**
+    * Returns extension name.
+    *
+    * @return string
+    */
+    public function getName()
+    {
+        return 'LinkToTwigExtension';
     }
 
-    $options = array_merge($raw, $options);
-
-    if(isset($options['page'])) {
-      $page_opts = array(
-        'href' => $options['page']->link(),
-        'content' => $options['page']->menu()
-      );
-      $options = array_merge($page_opts, $options);
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('link_to', [$this, 'linkTo']),
+        ];
     }
 
-    $options['content'] = isset($options['content']) ? $options['content'] : '';
-    
-    $blacklist = array('page', 'content');
-
-
-    foreach ($options as $key => $value) {
-      if(!in_array($key, $blacklist)) {
-        $options['attributes'][$key] = $value;
-      }
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('link_to', [$this, 'linkTo']),
+        ];
     }
 
-    return $this->formatLink($options);
-  }
+    public function linkTo($raw, $options = array())
+    {
+        switch (gettype($raw)) {
+            case 'object':
+                $raw = array('page' => $raw);
+                break;
+            case 'string':
+                $raw = array('content' => $raw);
+                break;
+            case 'array':
+                break;
+            default:
+                $raw = array();
+        }
 
-  private function formatLink($options) {
+        $options = array_merge($raw, $options);
 
-    return '<a' . $this->formatAttributes($options['attributes']) . '>' . $options['content'] . '</a>';
-  }
+        if (isset($options['page'])) {
+            $page_opts = array(
+                'href' => $options['page']->link(),
+                'content' => $options['page']->menu()
+            );
+            $options = array_merge($page_opts, $options);
+        }
 
-  private function formatAttributes($attributes) {
-    $attribute_string = '';
-    foreach ($attributes as $key => $value) {
-      $attribute_string .= ' ' . $key . '="' . $value . '"';
+        $options['content'] = isset($options['content']) ? $options['content'] : '';
+
+        $blacklist = array('page', 'content');
+
+
+        foreach ($options as $key => $value) {
+            if (!in_array($key, $blacklist)) {
+                $options['attributes'][$key] = $value;
+            }
+        }
+
+        return $this->formatLink($options);
     }
-    return $attribute_string;
-  }
+
+    private function formatLink($options)
+    {
+
+        return '<a' . $this->formatAttributes($options['attributes']) . '>' . $options['content'] . '</a>';
+    }
+
+    private function formatAttributes($attributes)
+    {
+        $attribute_string = '';
+        foreach ($attributes as $key => $value) {
+            $attribute_string .= ' ' . $key . '="' . $value . '"';
+        }
+        return $attribute_string;
+    }
 }
